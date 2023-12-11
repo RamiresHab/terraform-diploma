c6471ebb84b3e236bdacd12919aaab3df3ddafe2
Roman Khabibullin <ro.khabibullin@bi.zone>
2023-12-11 16:24:42 +0400

    add s3

--- provider.tf
+++ provider.tf
@@ -10,4 +10,18 @@ terraform {
       source = "yandex-cloud/yandex"
     }
   }
+
+  backend "s3" {
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+  bucket = "s3diplom-khabibullin"
+  region = "ru-central1"
+  key    = "diploma/terraform.tfstate"
+
+  skip_region_validation      = true
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true # необходимая опция Terraform для версии 1.6.1 и старше.
+  skip_s3_checksum            = true # необходимая опция при описании бэкенда для Terraform версии 1.6.3 и старше.
+  }
 }
